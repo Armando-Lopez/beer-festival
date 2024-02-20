f3007ca0412f2ee7719f40931bed16b4778e42ae
Diego López <d.lopez@homecapital.com.co>
2024-02-20 13:56:20 -0500

    change env

--- client/src/constants/api.js
+++ client/src/constants/api.js
@@ -1,3 +1,3 @@
-const BASE_API = 'http://localhost:5001/api'
-
+const BASE_API = process.env.NODE_ENV === 'production' ? '/api' :  'http://localhost:5001/api'
+console.log(BASE_API, process.env.NODE_ENV);
 export const SUBCRIBE_EMAIL_API_URL = `${BASE_API}/v1/subcribe`
